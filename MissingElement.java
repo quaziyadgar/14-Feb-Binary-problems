c1c34506943e072614c30803b673725b221bdc9c
quaziyadgar <quaziyadgar@github.com>
2022-02-16 20:48:38 +0530

    p

--- MissingElement.java
+++ MissingElement.java
@@ -1,10 +1,10 @@
 // 3. Given a sorted array of non-negative distinct integers, find the smallest missing non-negative element in it.
 
-//  Input: nums[] = [0, 1, 2, 6, 9, 11, 15]  Output: The smallest missing element is 3
+//  Input: numb[] = [0, 1, 2, 6, 9, 11, 15]  Output: The smallest missing element is 3
 
-//  Input: nums[] = [1, 2, 3, 4, 6, 9, 11, 15] Output: The smallest missing element is 0 .
+//  Input: numb[] = [1, 2, 3, 4, 6, 9, 11, 15] Output: The smallest missing element is 0 .
 
-// Input: nums[] = [0, 1, 2, 3, 4, 5, 6] Output: The smallest missing element is 7
+// Input: numb[] = [0, 1, 2, 3, 4, 5, 6] Output: The smallest missing element is 7
 
 public class MissingElement {
     public static void main(String[] args) {
